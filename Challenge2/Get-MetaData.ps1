6a129529f8e40ca356db7a1fbd16f1c22deb756d
Manish Bilung <manish.bilung@hotmail.com>
2021-04-05 03:41:52 +0530

    added if else clause

--- Challenge2/Get-MetaData.ps1
+++ Challenge2/Get-MetaData.ps1
@@ -1,33 +1,43 @@
 # Solution to Challege#2, query metadata of an instance within Azure
 
-if (Get-InstalledModule -Name "Az")
+# This script needs to be run through elevated PowerShell command prompt
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+function GetMetadata {
+    # Authenticate to Azure
+    Connect-AzAccount
+
+    # select subscription
+    Write-Output "Enter SubscriptionName:"
+    $SubscriptionName = Read-Host
+    Select-AzSubscription -SubscriptionName $SubscriptionName
+
+    # Get a list of all resource groups
+    Get-AzResourceGroup | Format-Table
+
+    # Get a list of all resources in a resource group
+    Write-Output "Enter ResourceGroupName:"
+    $ResourceGroupName = Read-Host
+    Get-AzResource -ResourceGroupName $ResourceGroupName | Format-Table
+
+    # Show resource meta data
+    Write-Output "Enter Name of resource to view it's metadata:"
+    $Name = Read-Host
+
+    # Converting result to JSON
+    Get-AzResource -Name $Name | ConvertTo-Json
+    pause
+}
+
+if (Get-InstalledModule -Name Azi)
     {
-        Write-Host "Az Module exists, You can authenticate to azure"    
+        Write-Host "Az Module exists, You can authenticate to azure"
+        GetMetadata        
     }
-else 
+# Install Az module if not installed
+else
     {
-        Write-Host "Az Module does not exist, Installing Az module"
+        Write-Host "Installing Az Module"
         Install-Module -Name Az -AllowClobber -SkipPublisherCheck
+        Write-Host "Az Module Installed, Executing the script"
+        GetMetadata
     }
-# Authenticate to Azure
-Connect-AzAccount
-
-# select subscription
-Write-Output "Enter SubscriptionName:"
-$SubscriptionName = Read-Host
-Select-AzSubscription -SubscriptionName $SubscriptionName
-
-# Get a list of all resource groups
-Get-AzResourceGroup | Format-Table
-
-# Get a list of all resources in a resource group
-Write-Output "Enter ResourceGroupName:"
-$ResourceGroupName = Read-Host
-Get-AzResource -ResourceGroupName $ResourceGroupName | Format-Table
-
-# Show resource meta data
-Write-Output "Enter Name of resource to view it's metadata:"
-$Name = Read-Host
-
-# Converting result to JSON
-Get-AzResource -Name $Name | ConvertTo-Json
