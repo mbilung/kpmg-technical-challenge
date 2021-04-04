c33f17216b2dc887d5473fef719a6c28def7468d
Manish Bilung <manish.bilung@hotmail.com>
2021-04-05 02:32:23 +0530

    Added solution to challenge2

--- Challenge2/Get-MetaData.ps1
+++ Challenge2/Get-MetaData.ps1
@@ -1,1 +1,24 @@
 # Solution to query metadata of an instance within Azure
+
+# Authenticate to Azure
+Connect-AzAccount
+
+# select subscription
+Write-Output "Enter SubscriptionName:"
+$SubscriptionName = Read-Host
+Select-AzSubscription -SubscriptionName $SubscriptionName
+
+# Get a list of all resource groups
+Get-AzResourceGroup | Format-Table
+
+# Get a list of all resources in a resource group
+Write-Output "Enter ResourceGroupName:"
+$ResourceGroupName = Read-Host
+Get-AzResource -ResourceGroupName $ResourceGroupName | Format-Table
+
+# Show resource meta data
+Write-Output "Enter Name of resource to view it's metadata:"
+$Name = Read-Host
+
+# Converting result to JSON
+Get-AzResource -Name $Name | ConvertTo-Json
